65389de34d05257c2233c6fcc7d6df7d500313d0
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-15 08:59:18 +0900

    BaseEntity 코드 정렬

--- src/main/java/com/project/board/entity/BaseEntity.java
+++ src/main/java/com/project/board/entity/BaseEntity.java
@@ -1,13 +1,13 @@
 package com.project.board.entity;
 
 import lombok.Getter;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.Modifying;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
