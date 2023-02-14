834142c23ec6523f186fef2549c79f54d511c3ff
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-15 08:50:02 +0900

    BoardRepository 테스트 코드 추가 및 수정
    - getBoardSoting() -> getBoardSorting() 으로 네이밍 수정
    - [QueryDSL] 검색 조건 테스트 - 좋아요 순 테스트 코드 추가

--- src/test/java/com/project/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/project/board/repository/BoardRepositoryTest.java
@@ -1,9 +1,6 @@
 package com.project.board.repository;
 
 import com.project.board.entity.Board;
-import com.project.board.entity.BoardTag;
-import com.project.board.entity.Tag;
-import org.apache.coyote.Request;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +29,7 @@ class BoardRepositoryTest {
     @Test
     void sortPageByDateTest(){
         // given
-        getBoardSoting();
+        getBoardSorting();
         // when
         PageRequest pageRequest =
                 PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "createdDate"));
@@ -53,7 +50,7 @@ class BoardRepositoryTest {
     @Test
     void sortPageByHitTest(){
         // given
-       getBoardSoting();
+       getBoardSorting();
         // when
         PageRequest pageRequest =
                 PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "hit"));
@@ -78,7 +75,7 @@ class BoardRepositoryTest {
     @Test
     void searchTest(){
         // given
-        getBoardSoting();
+        getBoardSorting();
         // when
         PageRequest pageRequest =
                 PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "createdDate"));
@@ -99,8 +96,35 @@ class BoardRepositoryTest {
                 .containsExactly(19,18,17,16,15);
 
     }
+   
+    @DisplayName("[QueryDSL] 검색 조건 테스트 - 좋아요 순")
+    @Test
+    void searchTest2(){
+        // given
+        getBoardSorting();
+        // when
+        PageRequest pageRequest =
+                PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC, "hit"));
 
-    private void getBoardSoting() {
+        Page<Board> page = boardRepository.searchBoard("제목1", pageRequest);
+        // then
+        assertThat(page.getSize()).isEqualTo(5);
+        assertThat(page.getTotalElements()).isEqualTo(11);
+        assertThat(page.getTotalPages()).isEqualTo(3);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.isLast()).isFalse();
+        List<Board> result = page.getContent();
+        assertThat(result).extracting("title")
+                .containsExactly("제목19","제목18","제목17","제목16","제목15");
+        assertThat(result).extracting("content")
+                .containsExactly("내용19","내용18","내용17","내용16", "내용15");
+        assertThat(result).extracting("hit")
+                .containsExactly(19,18,17,16,15);
+
+    }
+
+
+    private void getBoardSorting() {
         for(int i = 1; i <= 20; i ++){
             try{
                 Thread.sleep(50);
