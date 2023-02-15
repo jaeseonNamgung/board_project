e3ddeae682c9534ae31c6adaed0bf84fcd97c4bd
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-15 09:00:13 +0900

    BoardApplicationTests 코드 정렬

--- src/test/java/com/project/board/BoardApplicationTests.java
+++ src/test/java/com/project/board/BoardApplicationTests.java
@@ -1,7 +1,11 @@
 package com.project.board;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
 class BoardApplicationTests {
