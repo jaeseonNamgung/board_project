d21a716ac6ecfdd35f683db1b5dce7d7d4c90fc1
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-27 09:24:12 +0900

    부트 스트랩 코드 복사 및 여러 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into `board`(`TITLE`,`CONTENT`,`TAG_NAME`,`CATEGORY`,`COUNT_VISIT`,`HIT`, `CREATED_DATE`)
+values
+    ('스프링 관련 오륜', '스프링 오류', '#Spring#Jave', 'QUESTION_ANSWER',0,2, '2021-01-01 09:00:00'),
+    ('자바 관련 질문', '자바 질문 있습니다', '#Spring#Jave', 'QUESTION_ANSWER',0,1,'2021-01-01 13:00:00' ),
+    ('파이썬 관련 오류', '파이썬 오류', '#Spring#Jave', 'QUESTION_ANSWER',0,3,'2021-01-02 09:00:00'),
+    ('인텔리제이 관련 오류', '인텔리제이 오류', '#Spring#Jave', 'FREE_SUBJECT',0,6,'2021-01-03 09:00:00' ),
+    ('자바스크립트 질문', '자바스크립트 대해 이해가 안 갑니다.', '#Spring#Jave', 'FREE_SUBJECT',0,4,'2021-01-04 09:00:00'),
+    ('공부 관련 질문', '공부공부공부공부공부', '#Spring#Jave', 'FREE_SUBJECT',0,5, '2021-01-05 09:00:00'),
+    ('운동하자', '운동운동운동운동운동운동운동', '#Spring#Jave', 'FREE_SUBJECT',0,7, '2021-01-05 13:00:00'),
+    ('수업있다', '수업수업수업', '#Spring#Jave', 'STUDY',0,7, '2021-01-06 09:00:00'),
+    ('집가고싶다', '운동 오류', '#Spring#Jave', 'STUDY',0,7,'2021-01-06 13:00:00'),
+    ('나는', '운동 오류', '#Spring#Jave', 'STUDY',0,7, '2021-01-07 15:00:00');
+
+
+
+
+
